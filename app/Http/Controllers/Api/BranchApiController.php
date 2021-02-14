7bfc4fcf1b1c693b9323f1f35fea49f2bf06e6c8
khaingzinthant77 <55480270+khaingzinthant77@users.noreply.github.com>
2021-02-14 14:57:57 +0630

    [KZT]:update branch and department API

--- app/Http/Controllers/Api/BranchApiController.php
+++ app/Http/Controllers/Api/BranchApiController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Branch;
+use DB;
+
 
 class BranchApiController extends Controller
 {
     public function branch()
     {
-        $branches = Branch::where('status',1)->get();
-
+        // $branches = Branch::where('status',1)->get();
+    	$branches = Branch::with('employees')->get();
+        $branches = DB::table('branch')
+                     ->select('branch.name','branch.branch_color','branch.created_at','branch.updated_at','branch.latitude','branch.longitude','branch.status',DB::raw('count(employee.id) as total'))
+                     ->leftjoin('employee','employee.branch_id','branch.id')
+                     ->where('employee.name','!=','')
+                     ->groupBy('branch_id')
+                     ->get();
+        $branches = $branches->where('status',1);
         return response(['branches' => $branches,'message'=>"Successfully login",'status'=>1]);
     }
 }
+	
--- app/Http/Controllers/Api/DepartmentApiController.php
+++ app/Http/Controllers/Api/DepartmentApiController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Department;
+use DB;
 
 class DepartmentApiController extends Controller
 {
     public function department()
     {
-        $departments = Department::where('status',1)->get();
-
+        // $departments = Department::where('status',1)->get();
+        $department = Department::with('employees')->get();
+        $department = DB::table('department')
+                     ->select('department.name', 'department.dept_color','department.in_time','department.out_time','department.created_at','department.updated_at','department.status',DB::raw('count(employee.id) as total'))
+                     ->leftjoin('employee','employee.dep_id','department.id')
+                     ->where('employee.name','!=','')
+                     ->groupBy('dep_id')
+                     ->get();
+        $departments = $department->where('status',1);
         return response(['departments' => $departments,'message'=>"Successfully login",'status'=>1]);
     }
 }
