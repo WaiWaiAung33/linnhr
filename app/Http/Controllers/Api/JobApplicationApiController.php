49a8539ab1acef494d66aaa122ca7ceb7ec6156d
khaingzinthant77 <55480270+khaingzinthant77@users.noreply.github.com>
2021-02-20 14:11:05 +0630

    Update JobApplicationApiController.php

--- app/Http/Controllers/Api/JobApplicationApiController.php
+++ app/Http/Controllers/Api/JobApplicationApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\CvForm;
+use App\Cvform;
 use App\Department;
 use App\Position;
 use App\NRCCode;
