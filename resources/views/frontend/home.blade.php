86efb5bc0cf99d8cc610172f978fd6c4cc0e1386
WaiWaiAung33 <nawtoo1997@gmail.com>
2021-02-21 18:00:55 -0800

    [WWA]

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -168,7 +168,7 @@
                             </div>
                             <div class="popu-category-content">
                                 <h5 class="mb-2 text-dark title">{{$department->name}}</h5>
-                                <p class="text-success mb-0 rounded">{{$department->viewJobopening->count()}}</p>
+                                <p class="text-success mb-0 rounded">{{$department->viewJobopening->count()}} jobs</p>
                             </div>
                         </div>
                     </a>
