402f144230796bbdc3902fdcf80730e336b76c40
khaingzinthant77 <55480270+khaingzinthant77@users.noreply.github.com>
2021-03-01 12:07:13 +0630

    Update LeaveApiController.php

--- app/Http/Controllers/Api/LeaveApiController.php
+++ app/Http/Controllers/Api/LeaveApiController.php
@@ -50,7 +50,7 @@ class LeaveApiController extends Controller
 		            $leave_applications = $leave_applications->where('employee.dep_id',$request->dept_id);
 		        }
 		        if ($request->date != '') {
-		        	$leave_applications = $leave_applications->whereDate('start_date','<=',date('Y-m-d',strtotime($request->attendance_date)))->whereDate('end_date','>=',date('Y-m-d',strtotime($request->attendance_date)));
+		        	$leave_applications = $leave_applications->whereDate('leave_applications.start_date','<=',date('Y-m-d',strtotime($request->attendance_date)))->whereDate('leave_applications.end_date','>=',date('Y-m-d',strtotime($request->attendance_date)));
 		        }
 		        
 
