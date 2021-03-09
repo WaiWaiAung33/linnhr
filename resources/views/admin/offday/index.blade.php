5678ae213ac45ab458e32427e5c8cc2acbb00921
Aung Soe Oo <aungsoeoo.94s@gmail.com>
2021-03-09 11:12:01 +0630

    [ASO]- add calendar view in day off list

--- resources/views/admin/offday/index.blade.php
+++ resources/views/admin/offday/index.blade.php
@@ -11,7 +11,10 @@
   $name = isset($_GET['name'])?$_GET['name']:'';
   $branch_id = isset($_GET['branch_id'])?$_GET['branch_id']:'';
   $dept_id = isset($_GET['dept_id'])?$_GET['dept_id']:'';
-  $date = isset($_GET['date'])?$_GET['date']:''; 
+  $date = isset($_GET['date'])?$_GET['date']:'';
+
+  $year = isset($_GET['year'])?$_GET['year']:date('Y'); 
+  $month = isset($_GET['month'])?$_GET['month']:date('F');  
  
 ?>
 
@@ -100,9 +103,15 @@
                                     @endforeach
                                 </select>
                               </div>
-                              <div class="col-md-4">               
+                              {{-- <div class="col-md-4">               
                                   <input type="text" name="date" id="date" value="{{ old('date',$date) }}" class="form-control" style="font-size: 13px" placeholder="01-01-2021">
-                             </div>
+                              </div> --}}
+                              <div class="col-md-2">   
+                                 <input type="text" name="month" id="month"class="form-control unicode" placeholder="June" value="{{ old('month',$month) }}" style="font-size: 13px">
+                              </div>
+                              <div class="col-md-2">   
+                                 <input type="text" name="year" id="year"class="form-control unicode" placeholder="2021" value="{{ old('year',$year) }}" style="font-size: 13px">
+                              </div>
                             
                           </div>
                           <br>
@@ -126,75 +135,161 @@
       </div>
 
 
-    <p>Total record: {{$count}}</p>
+      <div class="tab">
+   <button class="tablinks" id="tbl_view" onclick="openTab(event, 'table_view')">Table View</button>
+   <button class="tablinks" onclick="openTab(event, 'calendar_view')">Calendar View</button>
+</div>
+<div id="table_view" class="tabcontent">
     <div class="table-responsive" style="font-size:14px">
-                <table class="table table-bordered styled-table">
-                  <thead>
-                    <tr> 
-                      <th>No</th>
-                      <th>Image</th>
-                        <th>Employee Name</th>
-                        <th>Off day 1</th>
-                        <th>Off day 2</th>
-                        <th>Off day 3</th>
-                        <th>Off day 4</th>
-                         <th>Last Updated by</th>
-                        <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                	@if($offdays->count()>0)
-            		  @foreach($offdays as $offday)
-                      <tr class="table-tr" data-url="{{route('offday.show',$offday->emp_id)}}">
-                          <td>{{++$i}}</td>
-                           @if($offday->photo == '')
-                          <td>
-                          <img src="{{ asset('uploads/employeePhoto/default.png') }}" alt="photo" width="80px" height="80px">
-                          </td>
-                          @else
-                          <td>
-                           <img src="{{ asset('uploads/employeePhoto/'.$offday->photo) }}" alt="photo" width="80px" height="80px">
-                           </td>
-                           @endif
-                            <td>{{$offday->viewEmployee->name}}<br>
-                              {{$offday->department_name}}<br>
-                              {{$offday->branch_name}}</td>
-                            <td>@if($offday->off_day_1!='') {{date('d-m-Y',strtotime($offday->off_day_1))}} @endif</td>
-                            <td>@if($offday->off_day_2!='') {{date('d-m-Y',strtotime($offday->off_day_2))}} @endif</td>
-                            <td>@if($offday->off_day_3!='') {{date('d-m-Y',strtotime($offday->off_day_3))}} @endif</td>
-                            <td>@if($offday->off_day_4!='') {{date('d-m-Y',strtotime($offday->off_day_4))}} @endif</td>
-                            <td>{{$offday->name}}</td>
-                            <td>
-                                <form action="{{route('offday.destroy',$offday->id)}}" method="post"
-                                    onsubmit="return confirm('Do you want to delete?');">
-                                   @csrf
-                                   @method('DELETE')
-                                    <a class="btn btn-sm btn-primary" href="{{route('offday.edit',$offday->id)}}"><i class="fa fa-fw fa-edit"></i></a>
-                                    <button class="btn btn-sm btn-danger btn-sm" type="submit">
-                                        <i class="fa fa-fw fa-trash" title="Delete"></i>
-                                    </button>
-                                </form>
-                            </td>
-                         </tr>
-                      @endforeach
-                        @else
-                            <tr align="center">
-                              <td colspan="10">No Data!</td>
-                            </tr>
-                    @endif
-		            
-                  </tbody>
-                </table> 
-           {!! $offdays->appends(request()->input())->links() !!}
-       </div>   
+       <table class="table table-bordered styled-table">
+          <thead>
+             <tr>
+                <th>No</th>
+                <th>Image</th>
+                <th>Employee Name</th>
+                <th>Day Off 1</th>
+                <th>Day Off 2</th>
+                <th>Day Off 3</th>
+                <th>Day Off 4</th>
+                <th>Last Updated by</th>
+                <th>Action</th>
+             </tr>
+          </thead>
+          <tbody>
+             @if($offdays->count()>0)
+             @foreach($offdays as $offday)
+             <tr class="table-tr" data-url="{{route('offday.show',$offday->emp_id)}}">
+                <td>{{++$i}}</td>
+                @if($offday->photo == '')
+                <td>
+                   <img src="{{ asset('uploads/employeePhoto/default.png') }}" alt="photo" width="80px" height="80px">
+                </td>
+                @else
+                <td>
+                   <img src="{{ asset('uploads/employeePhoto/'.$offday->photo) }}" alt="photo" width="80px" height="80px">
+                </td>
+                @endif
+                <td>{{$offday->viewEmployee->name}}<br>
+                   {{$offday->department_name}}<br>
+                   {{$offday->branch_name}}
+                </td>
+                <td>@if($offday->off_day_1!='') {{date('d-m-Y',strtotime($offday->off_day_1))}} @endif</td>
+                <td>@if($offday->off_day_2!='') {{date('d-m-Y',strtotime($offday->off_day_2))}} @endif</td>
+                <td>@if($offday->off_day_3!='') {{date('d-m-Y',strtotime($offday->off_day_3))}} @endif</td>
+                <td>@if($offday->off_day_4!='') {{date('d-m-Y',strtotime($offday->off_day_4))}} @endif</td>
+                <td>{{$offday->name}}</td>
+                <td>
+                   <form action="{{route('offday.destroy',$offday->id)}}" method="post"
+                      onsubmit="return confirm('Do you want to delete?');">
+                      @csrf
+                      @method('DELETE')
+                      <a class="btn btn-sm btn-primary" href="{{route('offday.edit',$offday->id)}}"><i class="fa fa-fw fa-edit"></i></a>
+                      <button class="btn btn-sm btn-danger btn-sm" type="submit">
+                      <i class="fa fa-fw fa-trash" title="Delete"></i>
+                      </button>
+                   </form>
+                </td>
+             </tr>
+             @endforeach
+             @else
+             <tr align="center">
+                <td colspan="10">No Data!</td>
+             </tr>
+             @endif
+          </tbody>
+       </table>
+       <div class="row">
+         <div class="col-md-12 text-center"><p>Total record: {{$count}}</p></div>
+       </div>
+       {!! $offdays->appends(request()->input())->links() !!}
+    </div>
+</div>
+<div id="calendar_view" class="tabcontent">
+   <div class="calendar" id="calendar"></div>
+</div>  
 
 @stop 
 @section('css')
 <link id="bsdp-css" href="{{ asset('/css/bootstrap-datepicker3.min.css')}}" rel="stylesheet">
+<link href='{{ asset("calender/fullcalendar.min.css") }}' rel='stylesheet' />
+<link href='{{ asset("calender/fullcalendar.print.min.css") }}' rel='stylesheet' media='print' />
+<style>
+  /* Style the tab */
+   .tab {
+     overflow: hidden;
+     border: 1px solid #ccc;
+     background-color: #f1f1f1;
+   }
+   /* Style the buttons inside the tab */
+   .tab button {
+     background-color: inherit;
+     float: left;
+     border: none;
+     outline: none;
+     cursor: pointer;
+     padding: 4px 6px;
+     transition: 0.3s;
+     font-size: 14px;
+   }
+   /* Change background color of buttons on hover */
+   .tab button:hover {
+    background-color: #ddd;
+   }
+   /* Create an active/current tablink class */
+   .tab button.active {
+    background-color: #ccc;
+   }
+   /* Style the tab content */
+   .tabcontent {
+     display: none;
+     padding: 6px 12px;
+     border: 1px solid #ccc;
+     border-top: none;
+   }
+    #calendar {
+        max-width: 75%;
+        margin: 0 auto;
+        font-family:Pyidaungsu,Yunghkio,'Masterpiece Uni Sans' !important;
+        font-size: 13px;
+    }
+    .fc-content{
+      color: white;
+    }
+</style>
 @stop
-
+<?php
+    $arr1 = []; 
+    $arr0 = [];
+    foreach($emp_offday_arr as $emp_offday){
+        if($emp_offday['off_day_1']!=''){
+          $a = ['title'=>$emp_offday['empname'], 'start'=> $emp_offday['off_day_1'] ];
+          array_push($arr1, $a);
+          array_push($arr0,$a);
+        }
+        if($emp_offday['off_day_2']!=''){
+          $a = ['title'=>$emp_offday['empname'], 'start'=> $emp_offday['off_day_2'] ];
+          array_push($arr1, $a);
+          array_push($arr0,$a);
+        }
+        if($emp_offday['off_day_3']!=''){
+          $a = ['title'=>$emp_offday['empname'], 'start'=> $emp_offday['off_day_3'] ];
+          array_push($arr1, $a);
+          array_push($arr0,$a);
+        }
+        if($emp_offday['off_day_4']!=''){
+          $a = ['title'=>$emp_offday['empname'], 'start'=> $emp_offday['off_day_4'] ];
+          array_push($arr1, $a);
+          array_push($arr0,$a);
+        }
+       
+    }
+    $date = date('Ymd')."";
+?>
 @section('js')
+<script src="{{ asset('js/jquery.min.js') }}"></script> 
 <script src="{{ asset('/js/bootstrap-datepicker.min.js')}}"></script>
+<script src='{{ asset("calender/moment.min.js") }}'></script>
+<script src='{{ asset("calender/fullcalendar.min.js") }}'></script>
 <script> 
     @if(Session::has('success'))
             toastr.options =
@@ -228,6 +323,16 @@
 
             //     this.form.submit();
             // });
+            // 
+            $("#year").datepicker({  format: "yyyy",
+               viewMode: "years", 
+               minViewMode: "years" 
+            });
+   
+             $("#month").datepicker({  format: "MM",
+                 viewMode: "months", 
+                 minViewMode: "months" 
+             });
    
         });
 
@@ -240,5 +345,32 @@
         $("#date").datepicker({ format: 'dd-mm-yyyy' });
 
         });
+
+        $(document).ready(function(){
+         document.getElementById("table_view").style.display = "block"; 
+         $("#tbl_view").addClass("active");       
+       });
+  
+       function openTab(evt, tabName) {
+   
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+            }
+         
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(tabName).style.display = "block";
+            evt.currentTarget.className += " active";
+          $('#calendar').fullCalendar({
+              defaultDate: new Date(),
+              editable: true,
+              eventLimit: true, // allow "more" link when too many events
+              events: <?php echo json_encode($arr1); ?>
+          });
+      }
      </script>
 @stop
